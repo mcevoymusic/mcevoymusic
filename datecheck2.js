4cf195f02d3a081ed4c838a860fbfc35ee98fa2a
mcevoymusic <157783700+mcevoymusic@users.noreply.github.com>
2024-04-16 16:26:09 +0800

    Update datecheck2.js

--- datecheck2.js
+++ datecheck2.js
@@ -3,7 +3,7 @@ function checkDate() {
     const currentDate = new Date();
 
     // Set the target date to April 14
-    const targetDate = new Date(currentDate.getFullYear(), 3, 18); // Note: Month is zero-based (0 = January, 3 = April)
+    const targetDate = new Date(currentDate.getFullYear(), 3, 12); // Note: Month is zero-based (0 = January, 3 = April)
 
     // Compare the current date with the target date
     if (currentDate > targetDate) {
