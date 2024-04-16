7946c178b17dca0089b3552f441ad7da7d651d76
mcevoymusic <157783700+mcevoymusic@users.noreply.github.com>
2024-04-16 16:04:18 +0800

    Add files via upload

---
+++ datecheck.js
@@ -0,0 +1,14 @@
+<script>
+    // Get the current date
+    const currentDate = new Date();
+
+    // Set the target date to April 14
+    const targetDate = new Date(currentDate.getFullYear(), 3, 18); // Note: Month is zero-based (0 = January, 3 = April)
+
+    // Compare the current date with the target date
+    if (currentDate > targetDate) {
+        document.getElementById('valid').style.display = 'none';
+    } else {
+        document.getElementById('error').style.display = 'none';
+    }
+</script>
